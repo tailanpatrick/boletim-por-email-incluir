0c5df376d33ebc987beb344a8a96c26d16353875
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2024-09-06 19:36:53 -0300

    fix após verficação de duplicatas por conta de acentos

--- services/CreateReportCardService.ts
+++ services/CreateReportCardService.ts
@@ -3,44 +3,40 @@ import StudentData from "@/types/StudentData";
 
 class CreateReportCardService {
     async execute(student: StudentData, pdfBuffer: Buffer) {
-       
         if (!student.name || !pdfBuffer) {
-            throw new Error('Estudante e seu boletim não enviado');
+            throw new Error('Estudante e boletim não enviado.');
         }
 
-        // Verifica quantos boletins o aluno já tem
-        const existingReportCardsCount = await prismaClient.student.count({
-            where: {
-                email: student.email,
-                name: student.name,
-            },
-        });
-
-        if (existingReportCardsCount >= 2) {
-            throw new Error('Aluno já possui boletins para dois cursos.');
-        }
-
-        // Normaliza o nome do curso (remove acentos)
+        // Normaliza o nome do curso para remover acentos apenas para a verificação
         const normalizedCourse = student.course.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 
-        // Verifica se já existe boletim salvo para este curso
-        const existingReportCardForCourse = await prismaClient.student.findFirst({
+        
+        const existingReportCards = await prismaClient.student.findMany({
             where: {
                 email: student.email,
                 name: student.name,
-                course: normalizedCourse,
             },
         });
 
-        if (existingReportCardForCourse) {
+        // Verifica se já existe boletim salvo para o curso removendo acentos na comparação
+        const courseExists = existingReportCards.some((reportCard) => {
+            const normalizedStoredCourse = reportCard.course.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+            return normalizedStoredCourse === normalizedCourse;
+        });
+
+        if (courseExists) {
             throw new Error(`Boletim já existente para o curso ${student.course}.`);
         }
 
-        // Cria o boletim
+ 
+        if (existingReportCards.length >= 2) {
+            throw new Error('Aluno já possui boletins para dois cursos.');
+        }
+
         const studentWithReportCard = await prismaClient.student.create({
             data: {
                 name: student.name,
-                course: student.course,
+                course: student.course, // Armazena o curso com acentos
                 semester: student.semester,
                 presence: student.presence,
                 percentPresence: student.percentPresence,
