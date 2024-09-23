784b398c238c99422b31221abdb43409ddf4ff58
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2024-09-23 19:55:06 -0300

    adicionado funcionalidade de download no botão Download do Template

--- components/ui/DownloadTemplate.tsx
+++ components/ui/DownloadTemplate.tsx
@@ -1,10 +1,20 @@
+import { LiaBuyNLarge } from "react-icons/lia";
 import Button from "./Button";
 
 const DownloadTemplate = () => {
+    const handleDownload = () => {
+        const link = document.createElement('a');
+        link.href = 'static/templatexlsx/[Curso] - [Semestre].xlsx';
+        link.download = '[Curso] - [Semestre].xlsx';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+
     return ( <div className="flex flex-col items-center">
         <p className="py-2 pt-8 font-bold">Não possui o template? Faça o Download do Template da Planilha</p>
 
-        <Button loading={false} onClick={()=> {}}>Download do Template</Button>
+        <Button loading={false} onClick={handleDownload}>Download do Template</Button>
 
         <p className="pt-12 pb-3 font-bold">Já possui a planilha padronizada?</p>
     </div> );
