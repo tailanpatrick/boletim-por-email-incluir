04b3cfd4874db02fd977c7667e51819f34117f73
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2024-09-24 11:31:03 -0300

    adicionado favicon incluir

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
