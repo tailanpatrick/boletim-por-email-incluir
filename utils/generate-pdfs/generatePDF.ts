5187293bf7f2e1ea393933fa14080922501ed21e
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2024-09-26 16:54:44 -0300

    ajustada a largura do boletim para mobile

--- utils/generate-pdfs/generatePDF.ts
+++ utils/generate-pdfs/generatePDF.ts
@@ -6,13 +6,26 @@ async function generatePDF(studentId: string): Promise<Buffer> {
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
+
+  await page.setViewport({
+    width: 400,
+    height: 1100,
+  });
+
   const url = `http://localhost:3000/report/${studentId}`;
 
   await page.goto(url, { waitUntil: "networkidle2", timeout: 60000 });
 
   const pdfBuffer = await page.pdf({
     printBackground: true,
-    format: "A4",
+    width:'400px',
+    margin: {
+      top: '0mm',          // Margem superior
+      bottom: '0mm',       // Margem inferior
+      left: '0mm',         // Margem esquerda
+      right: '0mm'
+    },
+    height: '1100px'
   });
 
   await browser.close();
