432849d3afd70906402833ac0b79a9e14f75a605
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2024-09-28 12:11:39 -0300

    Adicionado is loading a home, para controlar o loading da geração dos boletins

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,23 @@ import { useState } from "react";
 import FileUpload from "@/components/ui/FileUpload";
 import Navbar from "@/components/Navbar";
 import DownloadTemplate from "@/components/ui/DownloadTemplate";
-import Button from "@/components/ui/Button";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { toastSuccess } from "@/components/ui/Toast";
 
 export default function Home() {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [showDownloadComponent, setShowDownloadComponent] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const promise = new Promise<void>(resolve => setTimeout(()=> {
+    setIsLoading(false);
+    resolve();
+  }, 50000))
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+  
+    setIsLoading(true);
 
     if (!selectedFile) {
       alert("Por favor, selecione um arquivo.");
@@ -32,8 +41,10 @@ export default function Home() {
         alert(`Erro: ${errorResponse.error || 'Ocorreu um erro desconhecido'}`);
         return;
       }
+      await promise;
+      toastSuccess('Boletins Gerados com sucesso.')
+      setIsLoading(false);
 
-      alert("Boletins gerados e enviados com sucesso!");
     } catch (error: any) {
 
       alert(`Erro de rede: ${error.message as string}`);
@@ -42,16 +53,15 @@ export default function Home() {
 
   return (
     <>
-      <Navbar/>
-
+      <Navbar />
       <div className="flex flex-col h-full items-center justify-center text-center gap-5 md:p-10">
 
-      {showDownloadComponent && <DownloadTemplate/>}
+        {showDownloadComponent && <DownloadTemplate />}
         <form
           onSubmit={handleSubmit}
           className="flex flex-col h-full w-full items-center gap-4"
         >
-          <FileUpload onFileUpload={setSelectedFile} setShowDownloadComponent={setShowDownloadComponent} handleClickGenerateButton={handleSubmit}/>
+          <FileUpload onFileUpload={setSelectedFile} setShowDownloadComponent={setShowDownloadComponent} handleClickGenerateButton={handleSubmit} isLoading={isLoading} />
 
         </form>
       </div>
