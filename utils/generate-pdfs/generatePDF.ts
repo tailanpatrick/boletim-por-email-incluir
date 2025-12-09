eabd6dab0a77b9d1f2118821b03191836cd5f51b
Tailan Patrick <tailanpatrick06@gmail.com>
2025-12-09 15:16:27 -0300

    ajustado tempo de espera para gerar o pdf do boletim corretamente

--- utils/generate-pdfs/generatePDF.ts
+++ utils/generate-pdfs/generatePDF.ts
@@ -1,36 +1,42 @@
-const puppeteer = require("puppeteer");
+import { resolve } from 'path';
+
+const puppeteer = require('puppeteer');
 
 async function generatePDF(studentId: string): Promise<Buffer> {
-  const browser = await puppeteer.launch({
-    headless: false,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
-  const page = await browser.newPage();
+	const browser = await puppeteer.launch({
+		headless: false,
+		args: ['--no-sandbox', '--disable-setuid-sandbox'],
+	});
+	const page = await browser.newPage();
 
-  await page.setViewport({
-    width: 400,
-    height: 1120,
-  });
+	await page.setViewport({
+		width: 400,
+		height: 1120,
+	});
 
-  const url = `http://localhost:${process.env.PORT}/report/${studentId}`;
+	const url = `http://localhost:${process.env.PORT}/report/${studentId}`;
 
-  await page.goto(url, { waitUntil: "networkidle2", timeout: 90000 });
+	await page.goto(url, { waitUntil: 'networkidle2', timeout: 90000 });
 
-  const pdfBuffer = await page.pdf({
-    printBackground: true,
-    width:'400px',
-    margin: {
-      top: '0mm',          // Margem superior
-      bottom: '0mm',       // Margem inferior
-      left: '0mm',         // Margem esquerda
-      right: '0mm'
-    },
-    height: '1120px'
-  });
+	await page.evaluate(
+		() => new Promise((resolve) => setTimeout(resolve, 2000))
+	);
 
-  await browser.close();
+	const pdfBuffer = await page.pdf({
+		printBackground: true,
+		width: '400px',
+		margin: {
+			top: '0mm', // Margem superior
+			bottom: '0mm', // Margem inferior
+			left: '0mm', // Margem esquerda
+			right: '0mm',
+		},
+		height: '1120px',
+	});
 
-  return Buffer.from(pdfBuffer);
+	await browser.close();
+
+	return Buffer.from(pdfBuffer);
 }
 
 export default generatePDF;
